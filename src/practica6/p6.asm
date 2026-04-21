9de06adb15b6ef8638fda9d33d1872c3ff65acb0
JuanLagunaH <j2208686@uabc.edu.mx>
2026-04-21 21:11:47 +0000

    salvar cambios

--- src/practica6/p6.asm
+++ src/practica6/p6.asm
@@ -5,24 +5,45 @@ section	.text
 	global _start       ;referencia para inicio de programa
 	
 _start:                   
-
-    call getch
-    call itoa
     mov edx,ncad
     call puts
-
-    
 
     mov bx,word[len]
     mov edx,cad
     call capturar
-    mov al,[nlin]
+
+    mov al, 0xa
     call putchar
+    call putchar
+
+    ;MINUSCULAS
+    mov edx, msg_minus
     call puts
 
-    mov al,[nlin]
+    mov edx, cad
+    call minusculas 
+
+    mov edx, cad
+    call puts
+
+    mov al, 0xa
     call putchar
     call putchar
+
+    ;MAYUSCULAS 
+    mov edx, msg_mayus
+    call puts
+
+    mov edx, cad
+    call minusculas 
+
+    mov edx, cad
+    call puts
+
+    mov al, 0xa
+    call putchar
+    call putchar
+
 
 	mov	eax, 1	    	; seleccionar llamada al sistema para fin de programa
 	int	0x80        	; llamada al sistema - fin de programa
@@ -38,7 +59,7 @@ _start:
         jne .guardar
         call borrar
         jmp .ciclo
-       .guardar:
+        .guardar:
         call putchar
         mov [edx],al
         cmp al,0xa
@@ -63,24 +84,56 @@ _start:
         pop ax
         ret 
 
-    itoa:
-        push bx
-        mov bl,100
-        mov ah,0
-        div bl
-        mov bx,ax
-        add al,'0'
-        call putchar
-        mov al,ah
-        add al,'0'
-        call putchar
+    mayusculas:
+        push edx
+        push ax
 
+    .ciclomayus:
+        mov al, [edx]
+        cmp al, 0
+        je .salirmayus
+        cmp al, 'a'
+        jl .siguientemayus
+        cmp al, 'z'
+        jg .siguientemayus
+        sub al, 32
+        mov [edx], al
+    .siguientemayus:
+        inc edx
+        jmp .ciclomayus
 
-
-        pop bx
+    .salirmayus:
+        pop eax
+        pop edx
         ret
+
+    minusculas:
+        push edx
+        push ax
+
+    .ciclominus:
+        mov al, [edx]
+        cmp al, 0
+        je .salirminus
+        cmp al, 'A'
+        jl .siguienteminus
+        cmp al, 'Z'
+        jg .siguienteminus
+        add al, 32
+        mov [edx], al
+    .siguienteminus:
+        inc edx
+        jmp .ciclominus
+
+    .salirminus:
+        pop eax
+        pop edx
+        ret
+
 section	.data
-    ncad db 0xa,'Cadena: ',0
+    ncad db 0xa,'Ingrese una Cadena: ',0
     nlin db 0xa
+    msg_minus   db 'Minusculas: ', 0
+    msg_mayus   db 'Mayusculas: ', 0
     len db 64
     cad	times 64 db 0
